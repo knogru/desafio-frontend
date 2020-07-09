61ecddd0c6b32dd972cf759e9fdf0927439973f2
Aline Oliveira <oliveiralinester@gmail.com>
2020-07-09 18:19:31 -0300

    release

---
+++ build/precache-manifest.f0af2b7635be5b90939e0dfd2d367641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c68cdf4f1e52064ecf5d2e8005c3da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75bce9e6a38dc6ed0368",
+    "url": "/static/css/main.83efce1e.chunk.css"
+  },
+  {
+    "revision": "a20f216d687defc05c71",
+    "url": "/static/js/2.c538dd7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c538dd7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bce9e6a38dc6ed0368",
+    "url": "/static/js/main.9d64dd5b.chunk.js"
+  },
+  {
+    "revision": "dedc10634fc770a6ad9c",
+    "url": "/static/js/runtime-main.19e0e191.js"
+  }
+]);
