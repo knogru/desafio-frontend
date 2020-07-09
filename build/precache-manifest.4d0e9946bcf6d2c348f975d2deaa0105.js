8e2400c04d9241bffc923f90d8204924fbedc781
Aline Oliveira <oliveiralinester@gmail.com>
2020-07-09 18:29:34 -0300

    [fix] build

---
+++ build/precache-manifest.4d0e9946bcf6d2c348f975d2deaa0105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02ab575dca00528791f0c74c30f0da6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75bce9e6a38dc6ed0368",
+    "url": "./static/css/main.83efce1e.chunk.css"
+  },
+  {
+    "revision": "a20f216d687defc05c71",
+    "url": "./static/js/2.c538dd7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c538dd7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bce9e6a38dc6ed0368",
+    "url": "./static/js/main.9d64dd5b.chunk.js"
+  },
+  {
+    "revision": "8ecdffe881d16a6019b6",
+    "url": "./static/js/runtime-main.c64bd426.js"
+  }
+]);
